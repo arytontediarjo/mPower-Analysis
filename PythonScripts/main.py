04aecddb2b1b4f112707ccd6d18977a63ca72746
Aryton Tediarjo <atediarjo@gmail.com>
2019-09-25 15:58:30 -0700

    added argparse

--- PythonScripts/main.py
+++ PythonScripts/main.py
@@ -8,18 +8,31 @@ from multiprocessing import Pool
 import time
 import warnings
 from utils import getSynapseData
-from pdkit_features import pdkitFeaturize
-from spectral_flatness import sfm_auc
+from pdkit_features import pdkit_featurize
+from spectral_flatness import sfm_featurize
+import argparse
 warnings.simplefilter("ignore")
 
 
-def parallelize_dataframe(df, func):
-    
+## Read Arguments
+def read_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--filename", default= "data.csv",
+                        help = "Path for output results")
+    parser.add_argument("--num-cores", default= mp.cpu_count(),
+                        help = "Number of Cores, negative number not allowed")
+    parser.add_argument("--num-chunks", default= 10,
+                        help = "Number of sample per partition, no negative number")
+    parser.add_argument("--featurize", default= "spectral-flatness",
+                        help = "Features choices: 'pdkit' or 'spectral-flatness' ")
+    args = parser.parse_args()
+    return args
+
+def parallelize_dataframe(df, func, no_of_processors):
     ### split dataframe into 250 partitions ###
     df_split = np.array_split(df, 250)
     ### instantiate 16 processors as EC2 instance has 8 cores ###
-    no_of_processors = mp.cpu_count()*2
-    print("Number of processors: {}".format(no_of_processors))
+    print("Currently running on {} processors".format(no_of_processors))
     pool = Pool(no_of_processors)
     ### map function into each pools ###
     map_values = pool.map(func, df_split)
@@ -30,21 +43,6 @@ def parallelize_dataframe(df, func):
     pool.join()
     return df
 
-
-def featurize(data):
-    
-    for coord in ["x", "y", "z", "AA"]:
-        
-        ##### uncomment for pdkit
-        # data["accel_outbound_features_{}".format(coord)] = data["accel_outbound_pathfile"].apply(pdkitFeaturize, var = coord)
-        # data["accel_return_features_{}".format(coord)] = data["accel_return_pathfile"].apply(pdkitFeaturize,  var = coord)
-        # data["accel_resting_features_{}".format(coord)] = data["accel_rest_pathfile"].apply(pdkitFeaturize,  var = coord)
-        # data["userAccel_outbound_features_{}".format(coord)] = data["deviceMotion_outbound_pathfile"].apply(pdkitFeaturize, var = coord)
-        # data["userAccel_return_features_{}".format(coord)] = data["deviceMotion_return_pathfile"].apply(pdkitFeaturize, var = coord)
-        # data["userAccel_resting_features_{}".format(coord)] = data["deviceMotion_rest_pathfile"].apply(pdkitFeaturize, var = coord)
-        data["sfm_auc_{}".format(coord)] =  data["deviceMotion_rest_pathfile"].apply(sfm_auc, var = coord)
-    return data
-
 def getHealthCodeInfo(syn, synid):
     entity = syn.get(synid)
     df = pd.read_csv(entity["path"], sep = "\t")
@@ -53,7 +51,11 @@ def getHealthCodeInfo(syn, synid):
     return healthcode_subset, filtered_healthcode_df
 
 def main():
-    
+    ## Retrieve Arguments
+    args = read_args()
+    filename = args.filename
+    cores = args.num_cores
+    features = args.feature_choice
     
     ## login
     syn = sc.login()
@@ -61,16 +63,27 @@ def main():
     healthcode_subset, filtered_healthcode_df = getHealthCodeInfo(syn, "syn8381056")
     ## process data ##
     data = getSynapseData(syn, healthcode_subset)
-    data = parallelize_dataframe(data, featurize)
+    
+    
+    ## condition on choosing which features
+    print("Retrieving {} Features".format(features))
+    if features == "spectral-flatness":
+        data = parallelize_dataframe(data, sfm_featurize, cores)
+    elif features == "pdkit":
+        data = parallelize_dataframe(data, pdkit_featurize, features)
+    
     data = pd.merge(data, filtered_healthcode_df, how = "left", on = "healthCode")
     data = data[[feat for feat in data.columns if "path" not in feat]]
     
     
-    ## save data to synapse ##
-    data.to_csv("~/spectral_flatness_v2.csv")
-    new_file = File(path = "~/spectral_flatness_v2.csv", parentId = "syn20816722")
+    ## save data to local directory then to synapse ##
+    file_path = "~/{}".format(filename)
+    data.to_csv(file_path)
+    new_file = File(path = file_path, parentId = "syn20816722")
     new_file = syn.store(new_file)
     
+    
+## Run Main Function
 if __name__ == "__main__":
     start_time = time.time()
     main()
