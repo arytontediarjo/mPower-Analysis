84994268424b4159fcc124182201b293d5d82f4d
Aryton Tediarjo <atediarjo@sageitmbp1.hitronhub.home>
2019-12-07 14:13:39 -0800

    fixed shell scripts

--- create_data.sh
+++ create_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-python src/extract_raw_walking_features.py --version MPOWER_V1 --featurize pdkit --filename pdkit_mpower_v1.csv 
-python src/extract_raw_walking_features.py --version MPOWER_V2 --featurize pdkit --filename pdkit_mpower_v2.csv 
-python src/extract_raw_walking_features.py --version MPOWER_PASSIVE --featurize pdkit --filename pdkit_mpower_passive.csv 
-python src/extract_raw_walking_features.py --version MS_ACTIVE --featurize pdkit --filename pdkit_mpower_elevateMS.csv 
+python src/extract_raw_walking_features.py --version MPOWER_V1 --featurize pdkit --filename raw_pdkit_mpower_v1.csv 
+python src/extract_raw_walking_features.py --version MPOWER_V2 --featurize pdkit --filename raw_pdkit_mpower_v2.csv 
+python src/extract_raw_walking_features.py --version MPOWER_PASSIVE --featurize pdkit --filename raw_pdkit_mpower_passive.csv 
+python src/extract_raw_walking_features.py --version MS_ACTIVE --featurize pdkit --filename raw_pdkit_mpower_elevateMS.csv 
 python src/clean.py
 
