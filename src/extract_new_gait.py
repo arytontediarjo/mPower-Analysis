0a830782010fe0df5199cd20883d92c521341af6
Aryton Tediarjo <atediarjo@sageitmbp1.hitronhub.home>
2020-01-17 09:12:00 -0800

    fixed

--- src/extract_new_gait.py
+++ src/extract_new_gait.py
@@ -76,7 +76,7 @@ def main():
 
     ## create rotation file through multiprocessing jobs ## 
     rotation_data = query.parallel_func_apply(data, gproc.rotation_featurize_wrapper, 16, 250) 
-    rotation_data = rotation_data[rotation_data["gait.rotational_features"] != "#ERROR"]
+    rotation_data = rotation_data[rotation_data["gait.rotation_features"] != "#ERROR"]
     rotation_data = query.normalize_list_dicts_to_dataframe_rows(rotation_data, ["gait.rotation_features"])
     metadata_feature = ['recordId', 'healthCode','appVersion', 'phoneInfo', 'createdOn', 'test_type']
     rotation_feature = [feat for feat in rotation_data.columns if "rotation." in feat]
