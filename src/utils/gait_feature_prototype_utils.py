e1c30c6c07bf36f2b079c09c96942311e426af30
Aryton Tediarjo <atediarjo@sageitmbp1.hitronhub.home>
2020-01-16 23:02:01 -0800

    check code run

--- src/utils/gait_feature_prototype_utils.py
+++ src/utils/gait_feature_prototype_utils.py
@@ -306,25 +306,26 @@ def compute_pdkit_feature_per_window(data):
             {steps: some_value, ...}, ...]
     
     """
-    ts = data.copy()
-    window_size = 512
-    step_size   = 50
-    jPos        = window_size + 1
     ts_arr      = []
-    i           = 0
-    if len(ts) < jPos:
-        # print(ts.shape[0]/ts["td"][-1])
-        ts_arr.append(generate_pdkit_features_in_dict(ts))
-        return ts_arr
-    while jPos < len(ts):
-        jStart = jPos - window_size
-        subset = ts.iloc[jStart:jPos]
-        ts_arr.append(generate_pdkit_features_in_dict(subset)) 
-        jPos += step_size
-        i = i + 1
-    return [j for i in ts_arr for j in i]
+    for orientation in ["x", "y", "z", "AA"]:
+        ts = data.copy()
+        window_size = 512
+        step_size   = 50
+        jPos        = window_size + 1
+        i           = 0
+        if len(ts) < jPos:
+            # print(ts.shape[0]/ts["td"][-1])
+            ts_arr.append(generate_pdkit_features_in_dict(ts, orientation))
+            return ts_arr
+        while jPos < len(ts):
+            jStart = jPos - window_size
+            subset = ts.iloc[jStart:jPos]
+            ts_arr.append(generate_pdkit_features_in_dict(subset, orientation)) 
+            jPos += step_size
+            i = i + 1
+    return ts_arr
 
-def generate_pdkit_features_in_dict(data):
+def generate_pdkit_features_in_dict(data, orientation):
     """
     Function to generate pdkit features given orientation and time-series dataframe
     
@@ -342,107 +343,103 @@ def generate_pdkit_features_in_dict(data):
 
     """
 
-    feature_list = []
-    for orientation in ["x", "y", "z", "AA"]:
-        window_duration = data.td[-1] - data.td[0]
-        y_accel = data[orientation]
-        var = y_accel.var()
-        gp = pdkit.GaitProcessor(duration = window_duration,
-                                cutoff_frequency = 5,
-                                filter_order = 4,
-                                delta = 0.5)
-        try:
-            if (var) < 1e-3:
-                steps = 0
-                cadence = 0
-            else:
-                strikes, _ = gp.heel_strikes(y_accel)
-                steps      = np.size(strikes) 
-                cadence    = steps/window_duration
-        except:
-            steps = 0  
+    window_duration = data.td[-1] - data.td[0]
+    y_accel = data[orientation]
+    var = y_accel.var()
+    gp = pdkit.GaitProcessor(duration = window_duration,
+                            cutoff_frequency = 5,
+                            filter_order = 4,
+                            delta = 0.5)
+    try:
+        if (var) < 1e-3:
+            steps = 0
             cadence = 0
-        try:
-            peaks_data = y_accel.values
-            maxtab, _ = peakdet(peaks_data, gp.delta)
-            x = np.mean(peaks_data[maxtab[1:,0].astype(int)] - peaks_data[maxtab[:-1,0].astype(int)])
-            frequency_of_peaks = abs(1/x)
-            if np.isnan(frequency_of_peaks):
-                frequency_of_peaks = 0
-        except:
+        else:
+            strikes, _ = gp.heel_strikes(y_accel)
+            steps      = np.size(strikes) 
+            cadence    = steps/window_duration
+    except:
+        steps = 0  
+        cadence = 0
+    try:
+        peaks_data = y_accel.values
+        maxtab, _ = peakdet(peaks_data, gp.delta)
+        x = np.mean(peaks_data[maxtab[1:,0].astype(int)] - peaks_data[maxtab[:-1,0].astype(int)])
+        frequency_of_peaks = abs(1/x)
+        if np.isnan(frequency_of_peaks):
             frequency_of_peaks = 0
+    except:
+        frequency_of_peaks = 0
+    try:
+        speed_of_gait = gp.speed_of_gait(y_accel, wavelet_type='db3', wavelet_level=6)   
+    except:
+        speed_of_gait = 0
+    if steps >= 2:   # condition if steps are more than 2, during 2.5 seconds window 
+        step_durations = []
+        for i in range(1, np.size(strikes)):
+            step_durations.append(strikes[i] - strikes[i-1])
+        avg_step_duration = np.mean(step_durations)
+        sd_step_duration = np.std(step_durations)
+    else:
+        avg_step_duration = 0
+        sd_step_duration = 0
 
-        try:
-            speed_of_gait = gp.speed_of_gait(y_accel, wavelet_type='db3', wavelet_level=6)   
-        except:
-            speed_of_gait = 0
-        if steps >= 2:   # condition if steps are more than 2, during 2.5 seconds window 
-            step_durations = []
-            for i in range(1, np.size(strikes)):
-                step_durations.append(strikes[i] - strikes[i-1])
-            avg_step_duration = np.mean(step_durations)
-            sd_step_duration = np.std(step_durations)
-        else:
-            avg_step_duration = 0
-            sd_step_duration = 0
+    if steps >= 4:
+        strides1 = strikes[0::2]
+        strides2 = strikes[1::2]
+        stride_durations1 = []
+        for i in range(1, np.size(strides1)):
+            stride_durations1.append(strides1[i] - strides1[i-1])
+        stride_durations2 = []
+        for i in range(1, np.size(strides2)):
+            stride_durations2.append(strides2[i] - strides2[i-1])
+        strides = [strides1, strides2]
+        stride_durations = [stride_durations1, stride_durations2]
+        avg_number_of_strides = np.mean([np.size(strides1), np.size(strides2)])
+        avg_stride_duration = np.mean((np.mean(stride_durations1),
+                    np.mean(stride_durations2)))
+        sd_stride_duration = np.mean((np.std(stride_durations1),
+                    np.std(stride_durations2)))
+    else:
+        avg_number_of_strides = 0
+        avg_stride_duration = 0
+        sd_stride_duration = 0
 
-        if steps >= 4:
-            strides1 = strikes[0::2]
-            strides2 = strikes[1::2]
-            stride_durations1 = []
-            for i in range(1, np.size(strides1)):
-                stride_durations1.append(strides1[i] - strides1[i-1])
-            stride_durations2 = []
-            for i in range(1, np.size(strides2)):
-                stride_durations2.append(strides2[i] - strides2[i-1])
-            strides = [strides1, strides2]
-            stride_durations = [stride_durations1, stride_durations2]
-            avg_number_of_strides = np.mean([np.size(strides1), np.size(strides2)])
-            avg_stride_duration = np.mean((np.mean(stride_durations1),
-                        np.mean(stride_durations2)))
-            sd_stride_duration = np.mean((np.std(stride_durations1),
-                        np.std(stride_durations2)))
-        else:
-            avg_number_of_strides = 0
-            avg_stride_duration = 0
-            sd_stride_duration = 0
+    try:
+        step_regularity   = gp.gait_regularity_symmetry(y_accel,average_step_duration=avg_step_duration, 
+                                                    average_stride_duration=avg_stride_duration)[0]
+    except:
+        step_regularity   = 0
+    
+    try:
+        stride_regularity = gp.gait_regularity_symmetry(y_accel,average_step_duration=avg_step_duration, 
+                                                    average_stride_duration=avg_stride_duration)[1]
+    except:
+        stride_regularity = 0     
 
-        try:
-            step_regularity   = gp.gait_regularity_symmetry(y_accel,average_step_duration=avg_step_duration, 
-                                                        average_stride_duration=avg_stride_duration)[0]
-        except:
-            step_regularity   = 0
-        
-        try:
-            stride_regularity = gp.gait_regularity_symmetry(y_accel,average_step_duration=avg_step_duration, 
-                                                        average_stride_duration=avg_stride_duration)[1]
-        except:
-            stride_regularity = 0     
-
-        try:
-            symmetry          =  gp.gait_regularity_symmetry(y_accel,average_step_duration=avg_step_duration, 
-                                                        average_stride_duration=avg_stride_duration)[2]       
-        except:
-            symmetry          = 0                                                                                                             
-        
-        feature_list.append({
-                "walking.window_duration"      : window_duration,
-                "walking.axis"                 : orientation,
-                "walking.energy_freeze_index"  : calculate_freeze_index(y_accel)[0],
-                "walking.avg_step_duration"    : avg_step_duration,
-                "walking.sd_step_duration"     : sd_step_duration,
-                "walking.steps"                : steps,
-                "walking.cadence"              : cadence,
-                "walking.frequency_of_peaks"   : frequency_of_peaks,
-                "walking.avg_number_of_strides": avg_number_of_strides,
-                "walking.avg_stride_duration"  : avg_stride_duration,
-                "walking.sd_stride_duration"   : sd_stride_duration,
-                "walking.speed_of_gait"        : speed_of_gait,
-                "walking.step_regularity"      : step_regularity,
-                "walking.stride_regularity"    : stride_regularity,
-                "walking.symmetry"             : symmetry
-        })
-    return feature_list
+    try:
+        symmetry          =  gp.gait_regularity_symmetry(y_accel,average_step_duration=avg_step_duration, 
+                                                    average_stride_duration=avg_stride_duration)[2]       
+    except:
+        symmetry          = 0                                                                                                             
+    
+    feature_dict = {
+            "walking.window_duration"      : window_duration,
+            "walking.axis"                 : orientation,
+            "walking.energy_freeze_index"  : calculate_freeze_index(y_accel)[0],
+            "walking.avg_step_duration"    : avg_step_duration,
+            "walking.sd_step_duration"     : sd_step_duration,
+            "walking.steps"                : steps,
+            "walking.cadence"              : cadence,
+            "walking.frequency_of_peaks"   : frequency_of_peaks,
+            "walking.avg_number_of_strides": avg_number_of_strides,
+            "walking.avg_stride_duration"  : avg_stride_duration,
+            "walking.sd_stride_duration"   : sd_stride_duration,
+            "walking.speed_of_gait"        : speed_of_gait,
+            "walking.step_regularity"      : step_regularity,
+            "walking.stride_regularity"    : stride_regularity,
+            "walking.symmetry"             : symmetry}
+    return feature_dict
 
 def calculate_freeze_index(data):
     """
